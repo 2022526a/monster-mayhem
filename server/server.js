f2b36566f15d75334ae670c43e5190455a7e4c0f
Ahmad Shabbir <itzahmadshabbirhere@gmail.com>
2025-06-04 18:48:01 +0500

    project setup commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const socketio = require('socket.io');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
